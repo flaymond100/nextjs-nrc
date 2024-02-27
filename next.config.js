43b9542ee96327d50248d862f1dbf64ada7a08b3
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-02-27 13:59:16 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   output: "export",
+  distDir: "out",
 };
 
 module.exports = nextConfig;
