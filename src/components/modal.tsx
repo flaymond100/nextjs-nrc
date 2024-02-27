bfe317aa40da89842040862fc2a8a2822eca38e1
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-02-27 15:21:33 +0100

    fix

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -29,10 +29,10 @@ function Modal() {
       console.log(values);
       emailjs
         .send(
-          process.env.SERVICE_ID!,
-          process.env.TEMPLATE_ID!,
+          process.env.NEXT_PUBLIC_SERVICE_ID!,
+          process.env.NEXT_PUBLIC_TEMPLATE_ID!,
           values,
-          process.env.PUBLIC_KEY!
+          process.env.NEXT_PUBLIC_PUBLIC_KEY!
         )
         .then(
           () => {
