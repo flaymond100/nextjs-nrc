dda7cbbf6aecdfddb53c4e192a99a98cd0df0d71
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-02-27 13:51:33 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
