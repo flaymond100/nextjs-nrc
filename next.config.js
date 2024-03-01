ae3c1324df352833b557b7fbde1918bbffcd4a7a
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-03-01 12:50:04 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   // Use the prefix for GitHub Pages
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   // assetPrefix: "/nextjs-nrc/",
   // Enable static export
   trailingSlash: true,
