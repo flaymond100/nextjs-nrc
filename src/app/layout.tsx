d0c4166622967ede82d7c2e98ea20bff1edc7df3
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-03-05 15:02:18 +0100

    Update metadata and schema for NRC Team website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,29 +14,14 @@ const roboto = Questrial({
   display: "swap",
 });
 
-// export const metadata: Metadata = {
-//   openGraph: {
-//     title: "NRC Team",
-//     siteName: "NRC Team",
-//     type: "website",
-//     emails: "contact@nrc-team.com",
-//     url: "https://www.nrc-team.com",
-//     locale: "en",
-//     description:
-//       "Individual training plans for triathlon, cycling and running.",
-//     images: {
-//       width: "500",
-//       height: "500",
-//       url: "https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Ffavicon.ico?alt=media&token=05c556ea-e2ff-4626-af20-69506cdea62b",
-//     },
-//   },
-//   title: "NRC Team",
-//   description:
-//     "NRC Team is a community of athletes from all corners of the globe, we provide individual training plans for triathlon, cycling and running.",
-//   alternates: {
-//     canonical: `https://www.nrc-team.com`,
-//   },
-// };
+export const metadata: Metadata = {
+  title: "NRC Team",
+  description:
+    "NRC Team is a community of athletes from all corners of the globe, we connect people and provide individual training plans for triathlon, cycling and running.",
+  alternates: {
+    canonical: `https://www.nrc-team.com`,
+  },
+};
 
 const schema = {
   "@context": "http://schema.org",
@@ -47,7 +32,7 @@ const schema = {
   start_url: "/",
   founder: "Konstantin Garbar",
   url: "https://www.nrc-team.com",
-  logo: "https://www.nrc-team.com/logo.png",
+  logo: "https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2FNRC_logo_web.jpg?alt=media&token=f9e70e4f-8c1b-4ecb-85d3-0a1b6df10aea",
   keywords:
     "Traithlon, Traithlon Coach, Traithlon Coaching, Cycling, Cycling Coaching, Germany, Berlin, Leipzig, Best Cycling Coach, Best Triathlon Coach, Training Camps, Racing, Triathlon, Cycling Germany, Triathlon Germany, Best Coach Germany, Best Coach Dubai, Trainin Plans, Triathlon Training Plan, Individual Triathlon Training Plan, Triathlon Training Plan, Ironman, Half Ironman, Sprint Triathlon, Bikes, Hour Record, Indoor Training, Zwift, Wahoo, Today's Plan, Training Peaks",
   sport: "triathlon",
@@ -75,6 +60,18 @@ export default function RootLayout({
           href={`${process.env.NEXT_PUBLIC_BASE_URL}/favicon.ico`}
         />
         <link rel="canonical" href="https://www.nrc-team.com" />
+        <meta
+          property="title"
+          content="NRC Team - Running, Cycling and Triathlon Trainings"
+        />
+        <meta
+          property="description"
+          content="NRC Team is a community of athletes from all corners of the globe, we connect people and provide individual training plans for triathlon, cycling and running."
+        />
+        <meta
+          property="image"
+          content="https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2FNRC_logo_web.jpg?alt=media&token=f9e70e4f-8c1b-4ecb-85d3-0a1b6df10aea"
+        />
         <meta
           property="og:title"
           content="NRC Team - Running, Cycling and Triathlon Trainings"
