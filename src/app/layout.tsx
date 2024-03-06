3801d3a595e97be6fa9073a42dc45619c9de4872
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-03-06 10:33:15 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,24 @@ export default function RootLayout({
           href={`${process.env.NEXT_PUBLIC_BASE_URL}/favicon.ico`}
         />
         <link rel="canonical" href="https://www.nrc-team.com" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
         <meta
           property="title"
           content="NRC Team - Running, Cycling and Triathlon Trainings"
