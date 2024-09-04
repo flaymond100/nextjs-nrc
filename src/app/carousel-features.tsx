4a11120ce5902a17f8221750ef0243d577cba6c6
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-09-04 15:45:15 +0200

    added carousel

--- src/app/carousel-features.tsx
+++ src/app/carousel-features.tsx
@@ -1,74 +1,195 @@
 "use client";
 
 import Image from "next/image";
-import { Typography, Carousel } from "@material-tailwind/react";
+import { Typography, Rating } from "@material-tailwind/react";
+import Marquee from "react-fast-marquee";
 
 export function CarouselFeatures() {
   return (
-    <div className="px-8 py-56">
-      <section className="container mx-auto !rounded-lg bg-[url(' /image/Background.png')] bg-center py-10 lg:px-16">
-        <Carousel
-          placeholder={""}
-          transition={{ duration: 1 }}
-          nextArrow={() => <></>}
-          prevArrow={() => <></>}
-          navigation={({ setActiveIndex, activeIndex, length }) => (
-            <div className="absolute left-16 bottom-0 z-50 flex h-5 w-20 -translate-x-2/4 gap-2 md:left-2/4">
-              {new Array(length).fill("").map((_, i) => (
-                <span
-                  key={i}
-                  className={`block h-1 w-10 cursor-pointer transition-all content-[''] ${
-                    activeIndex === i ? "bg-white" : "bg-white/50"
-                  }`}
-                  onClick={() => setActiveIndex(i)}
-                />
-              ))}
-            </div>
-          )}
-        >
-          {new Array(2).fill("").map((_, i) => (
-            <div
-              key={i}
-              className="!relative flex grid-cols-1 flex-col-reverse gap-6 px-10 py-14 md:grid md:grid-cols-5  md:gap-14 md:py-20"
+    <>
+      {/* <section
+        style={{
+          background: "rgb(55 0 125 / 91%)",
+        }}
+        className="p-16"
+      >
+        <div className="container mx-auto grid grid-cols-1 gap-y-12 md:grid-cols-2 lg:grid-cols-2">
+          <div className="px-8 text-center">
+            <Typography
+              placeholder=""
+              variant="h3"
+              color="white"
+              className="mb-6 font-medium"
             >
-              <div className="col-span-3 flex flex-col items-start justify-center">
-                <Typography
-                  placeholder={""}
-                  variant="lead"
-                  color="white"
-                  className="mb-5 text-xl font-normal "
-                >
-                  Easy Shopping, Quick Delivery <br />
-                  No need to stress about shopping for books. Order online and
-                  have your textbooks and supplies delivered straight to your
-                  doorstep for free.
-                </Typography>
-                <div className="flex items-center gap-2">
-                  🚚
-                  <Typography
-                    placeholder={""}
-                    variant="small"
-                    color="white"
-                    className="font-medium uppercase"
-                  >
-                    Free Delivery
-                  </Typography>
-                </div>
-              </div>
-              <div className="col-span-2 flex w-full shrink-0 md:!justify-end">
-                <Image
-                  width={768}
-                  height={768}
-                  src=" /image/logos/logo-amazon 3.svg"
-                  alt="testimonial image"
-                  className="h-full w-2/4 object-contain md:!w-2/3"
-                />
-              </div>
+              &quot;Thanks to this plan, I hit my PB in my last triathlon! The
+              coaches are incredibly supportive and responsive to my needs,
+              making it feel like I'm never training alone.&quot;
+            </Typography>
+            <Typography
+              placeholder=""
+              color="white"
+              variant="h4"
+              className="mt-4"
+            >
+              Oleg
+            </Typography>
+            <Typography
+              placeholder=""
+              color="white"
+              className="mb-4 font-normal"
+            >
+              Software Developer
+            </Typography>
+            <Rating placeholder="" value={5} readonly />
+          </div>
+          <div className="px-8 text-center">
+            <Typography
+              placeholder=""
+              variant="h3"
+              color="white"
+              className="mb-6 font-medium"
+            >
+              &quot;The level of detail and customization in these plans is
+              outstanding. They’ve helped me balance work, life, and training
+              without feeling overwhelmed.&quot;
+            </Typography>
+            <Typography
+              placeholder=""
+              color="white"
+              variant="h4"
+              className="mt-4"
+            >
+              Anna
+            </Typography>
+            <Typography
+              placeholder=""
+              color="white"
+              className="mb-4 font-normal"
+            >
+              Marketing Specialist
+            </Typography>
+            <Rating placeholder="" value={5} readonly />
+          </div>
+        </div>
+      </section> */}
+      <section
+        style={{
+          background: "rgb(55 0 125 / 91%)",
+        }}
+        className="flex p-8 justify-center items-center w-screen"
+      >
+        <div className=" rounded-2xl w-4/5">
+          <Marquee className="rounded-2xl overflow-hidden grid">
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5a5e0d75755dd4bf51_zwift-inc-logo-vector-2%402x.webp?alt=media&token=4af41770-98ee-4d9f-8f8d-95a119537883`}
+                alt="zwift logo"
+              />
             </div>
-          ))}
-        </Carousel>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5addac87c670d4c904_Wahoo%402x.webp?alt=media&token=e1b7e6b7-ebd4-486a-bde2-f42e474f081e`}
+                alt="wahoo logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={170}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530cc1e8c20d4b3f24c4f0_Strava_Logo%402x.webp?alt=media&token=bb489393-06d7-4ae7-b131-d0c3156d2f32`}
+                alt="strava logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center   mx-8 ">
+              <Image
+                width={150}
+                height={170}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65549b703d9a1a82ff6ceffc_garmin-logo.webp?alt=media&token=f6676ba3-2e0b-46bd-b377-d1f9cef4aff2`}
+                alt="garmin logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5a5e0d75755dd4bf51_zwift-inc-logo-vector-2%402x.webp?alt=media&token=4af41770-98ee-4d9f-8f8d-95a119537883`}
+                alt="zwift logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65549b703d9a1a82ff6ceff6_polar-logo.webp?alt=media&token=fdaa2eb2-fa90-4d12-996d-0704813f4e82`}
+                alt="polar logo"
+              />
+            </div>
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5a5e0d75755dd4bf51_zwift-inc-logo-vector-2%402x.webp?alt=media&token=4af41770-98ee-4d9f-8f8d-95a119537883`}
+                alt="zwift logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5addac87c670d4c904_Wahoo%402x.webp?alt=media&token=e1b7e6b7-ebd4-486a-bde2-f42e474f081e`}
+                alt="wahoo logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={170}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530cc1e8c20d4b3f24c4f0_Strava_Logo%402x.webp?alt=media&token=bb489393-06d7-4ae7-b131-d0c3156d2f32`}
+                alt="strava logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center   mx-8 ">
+              <Image
+                width={150}
+                height={170}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65549b703d9a1a82ff6ceffc_garmin-logo.webp?alt=media&token=f6676ba3-2e0b-46bd-b377-d1f9cef4aff2`}
+                alt="garmin logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65530c5a5e0d75755dd4bf51_zwift-inc-logo-vector-2%402x.webp?alt=media&token=4af41770-98ee-4d9f-8f8d-95a119537883`}
+                alt="zwift logo"
+              />
+            </div>
+
+            <div className=" flex flex-col justify-center items-center    mx-8 ">
+              <Image
+                width={150}
+                height={150}
+                src={`https://firebasestorage.googleapis.com/v0/b/nrc-team.appspot.com/o/files%2Fcarousel%2F65549b703d9a1a82ff6ceff6_polar-logo.webp?alt=media&token=fdaa2eb2-fa90-4d12-996d-0704813f4e82`}
+                alt="polar logo"
+              />
+            </div>
+          </Marquee>
+        </div>
       </section>
-    </div>
+    </>
   );
 }
 
