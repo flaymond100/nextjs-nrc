e3529b6878b32112748ad82f4ac9079c6f6c3d0d
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-11-01 20:33:22 +0100

    renaming

--- src/app/plans/layout.tsx
+++ src/app/plans/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Layout } from "@/components";
 
 export const metadata: Metadata = {
-  title: "Our Plans",
+  title: "Trainings",
   description:
     "Choose the most suitable progam for you - running training, cycling training or triathlon training.",
   alternates: {
