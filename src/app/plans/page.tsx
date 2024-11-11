26c5e71219fe4e3c11bddf1cdc4f059acec8d8b8
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2024-11-11 19:54:26 +0100

    up

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -8,11 +8,13 @@ import Faq from "../faq";
 import OurPrograms from "../our-programs";
 import CarouselFeatures from "../carousel-features";
 import { CalendlyWidget } from "@/components/widget";
+import { SocialRides } from "@/components/social-rides";
 
 export default function PricingPage() {
   return (
     <>
       <Navbar />
+      <SocialRides />
       <OurPrograms />
       <Prices />
       <CarouselFeatures />
