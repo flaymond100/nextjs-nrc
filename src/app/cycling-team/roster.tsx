a64be828c27a6dea746951b7d2a99bf74faf2957
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-05-27 12:27:37 +0200

    website update

--- src/app/cycling-team/roster.tsx
+++ src/app/cycling-team/roster.tsx
@@ -13,7 +13,7 @@ import {
 } from "@material-tailwind/react";
 import Image from "next/image";
 import Link from "next/link";
-import { BsInstagram, BsStrava } from "react-icons/bs";
+import { BsCompass, BsInstagram, BsStrava } from "react-icons/bs";
 export const Roster = () => {
   const riders = [
     {
@@ -62,11 +62,12 @@ export const Roster = () => {
     {
       id: 14,
       name: "Jan",
-      role: "",
+      role: "Coffee Ride Specialist",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Jan.webp`,
-      bio: "",
+      bio: "Shocked to learn that I need to start in the masters category not because I’ve mastered cycling — it just means my knees now creak louder than my freehub. It's less “King of the Mountain” and more “Lord of the Ibuprofen.” I still love cycling - miles are my meditation.",
+      stravaUrl: "https://www.strava.com/athletes/8125327",
     },
     {
       id: 10,
@@ -82,20 +83,26 @@ export const Roster = () => {
     {
       id: 9,
       name: "Lisa",
-      role: "",
+      role: "Gravel Grinder",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Lisa.webp`,
-      bio: "",
+      bio: "Off-road addict with a soft spot for climbs, teammates, and gummy bears. Racing ultras, chasing dirt, and modeling hearts when I’m not riding mine out.",
+      stravaUrl: "https://www.strava.com/athletes/38193512",
+      instagramUrl:
+        "https://www.instagram.com/lieschenpe?igsh=dWgwOWEwMHo0NzZ1&utm_source=qr",
     },
     {
       id: 8,
       name: "Paul",
-      role: "",
+      role: "All-rounder / Breakaway Specialist",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Paul.webp`,
-      bio: "",
+      bio: "I'm a rider who loves all kinds of races. I want to create breakaways and help my teammates the best I can.",
+      stravaUrl: "https://strava.app.link/AmKJhe1LHTb",
+      instagramUrl:
+        "https://www.instagram.com/pamecycling?igsh=MXA4dWszamIxbG5v&utm_source=qr",
     },
     {
       id: 7,
@@ -104,18 +111,19 @@ export const Roster = () => {
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Robert.webp`,
-      bio: "",
+      bio: "Crit lover with a need for speed and corners so tight they squeal. Full gas always, unless the road tilts up — then it’s survival mode. Built for watts, not altitude.",
       stravaUrl: "https://strava.app.link/33AFoyhoATb",
       instagramUrl: "https://www.instagram.com/smirrni?igsh=ZnhnenBya2Izcjdl",
     },
     {
       id: 6,
       name: "Simon",
-      role: "",
+      role: "All-rounder / Rouleur",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Simon.webp`,
-      bio: "",
+      bio: "Challenging races and tactical battles - this is where I feel at home. I also enjoy cycling in the mountains.",
+      stravaUrl: "https://strava.app.link/OoDLP06KHTb",
     },
     {
       id: 13,
@@ -131,20 +139,26 @@ export const Roster = () => {
     {
       id: 5,
       name: "Tina",
-      role: "",
+      role: "All-rounder",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Tina.webp`,
-      bio: "",
+      bio: "Whether asphalt or gravel - the main thing is movement and freedom, where the team spirit isn’t neglected.",
+      stravaUrl: "https://strava.app.link/PKBENRPEGTb",
+      instagramUrl: "https://www.instagram.com/tiri80",
     },
     {
       id: 4,
       name: "Tony",
-      role: "",
+      role: "All-rounder / Rouleur",
       photoUrl: `${
         process.env.NEXT_PUBLIC_BASE_URL ?? ""
       }/image/team-pictures/Tony.webp`,
-      bio: "",
+      bio: "`Business is like riding a bicycle, either you keep moving or you fall down.` ...So i stay tuned &  give everything I can",
+      stravaUrl: "https://strava.app.link/ZHkCjFQXGTb",
+      instagramUrl:
+        "https://www.instagram.com/tonyschurigt?utm_source=qr&igsh=MWRidWQ0ODlzanh2MQ==",
+      komootUrl: "https://www.komoot.de/user/3989840054079?ref=amk",
     },
     {
       id: 12,
@@ -168,17 +182,58 @@ export const Roster = () => {
       bio: "I love tackling short, steep climbs with explosive power and launching strong attacks to shake off my rivals and help my team.",
       stravaUrl: "https://strava.app.link/u0dNM7B5CTb",
     },
-  ];
+    {
+      id: 16,
+      name: "Lion",
+      role: "Rouleur/Sprinter",
+      photoUrl: `${
+        process.env.NEXT_PUBLIC_BASE_URL ?? ""
+      }/image/team-pictures/Lion.webp`,
+      bio: "Four corners and absolutely flat, that's my habitat, recently trying to get familiar with mountains.",
+      stravaUrl: "https://strava.app.link/bEse6o9zGTb",
+    },
+    {
+      id: 17,
+      name: "Lukas",
+      role: "Sprinter",
+      photoUrl: `${
+        process.env.NEXT_PUBLIC_BASE_URL ?? ""
+      }/image/team-pictures/Lukas.webp`,
+      bio: "I'm a sprinter who favours flat and slightly undulating profiles. Always ready for town sign sprints.",
+      stravaUrl: "https://strava.app.link/9URFuOzMGTb",
+    },
+    {
+      id: 18,
+      name: "Martin",
+      role: "Gravel & Bikepacking",
+      photoUrl: `${
+        process.env.NEXT_PUBLIC_BASE_URL ?? ""
+      }/image/team-pictures/Martin.jpg`,
+      bio: "I'm the hobby rider in the team. Some gucci gravel, small climbs, single trails in the forest and I'm happy.",
+      stravaUrl: "https://www.strava.com/athletes/19424135",
+      instagramUrl: "https://www.instagram.com/herr_bpunkt",
+      komootUrl: "https://www.komoot.com/de-de/user/376857048740",
+    },
+    {
+      id: 19,
+      name: "Max",
+      role: "All-rounder",
+      photoUrl: `${
+        process.env.NEXT_PUBLIC_BASE_URL ?? ""
+      }/image/team-pictures/Max.webp`,
+      bio: "Formerly a soccer player and fitness enthusiast, I've been trying my luck with the bike since last year (2024).",
+      stravaUrl: "https://www.strava.com/athletes/106788303",
+      instagramUrl: "https://www.instagram.com/mmmakz",
+    },
+  ].sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <section className="container mx-auto py-10 px-6">
       <h1 className="mb-10 text-center text-5xl font-bold">Team Squad</h1>
 
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {riders.map((rider) => (
-          <Card
-            key={rider.id}
-            className="shadow-lg mb-10 h-[45rem] flex flex-col"
-          >
+          <Card key={rider.id} className="shadow-lg mb-10  flex flex-col">
             {/* CardHeader for Rider Photo */}
             <CardHeader style={{ height: "27rem" }} className="relative">
               <Image
@@ -220,6 +275,31 @@ export const Roster = () => {
                       className="bg-gradient-to-tr from-yellow-500 via-pink-600 to-purple-700 hover:from-yellow-600 hover:via-pink-700 hover:to-purple-800"
                     >
                       <BsStrava className="text-white text-xl" />
+                    </Button>
+                  </Link>
+                )}
+                {rider.komootUrl && (
+                  <Link
+                    aria-label="Go to komoot"
+                    target="_blank"
+                    href={rider.komootUrl}
+                  >
+                    <Button
+                      placeholder={""}
+                      aria-label="Go to komoot"
+                      size="lg"
+                      name="Komoot"
+                      style={{ background: "#447c00" }}
+                      className="bg-gradient-to-tr from-#8acb3c via-#66ae0e to-#447c00 hover:from-yellow-600 hover:via-pink-700 hover:to-purple-800"
+                    >
+                      <Image
+                        src={`${
+                          process.env.NEXT_PUBLIC_BASE_URL ?? ""
+                        }/image/komoot.png`}
+                        alt="Komoot"
+                        width={20}
+                        height={20}
+                      />
                     </Button>
                   </Link>
                 )}
