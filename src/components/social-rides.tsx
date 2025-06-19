fe85eeb7a0ab239341f74fe6243b67a7b1e264b7
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-06-19 09:52:58 +0200

    add komooot

--- src/components/social-rides.tsx
+++ src/components/social-rides.tsx
@@ -6,19 +6,21 @@ import Image from "next/image";
 import { BsInstagram } from "react-icons/bs";
 
 export const SocialRides = () => {
+  const komootUrl =
+    "https://www.komoot.com/de-de/tour/2335614096?share_token=aZrwK1I7mQQLveYDYjrAzBeSZzcOuMU81NoPV2hGBp3qFUU9xk&ref=wtd";
   return (
     <div className="container text-center md:text-left mt-6 mb-10 md:mt-10 mx-auto">
       <h1 className="mb-8 mt-10 text-center text-4xl font-bold">
         {" "}
         Open Social Rides in Leipzig
       </h1>
-      <h1 className="mb-8 mt-10 text-center text-2xl font-bold text-red-600">
+      {/* <h1 className="mb-8 mt-10 text-center text-2xl font-bold text-red-600">
         {" "}
         <strong>NO Social Ride on 14 June!</strong> The team will be on the
         race.
         <br />
         We will be back on 21 June.
-      </h1>
+      </h1> */}
       <div className="container mx-auto  grid grid-cols-1 gap-6 lg:grid-cols-2 justify-items-center md:justify-items-end">
         <div className="animate-in slide-in-from-left duration-1000">
           <p className="leter-spacing-1 text-xl max-w-3xl mb-5">
@@ -26,25 +28,34 @@ export const SocialRides = () => {
             Leipzig, creating opportunities to connect and grow together in
             person.
           </p>
-          <h3 className="mb-2 text-2xl font-bold text-red-600">
-            ⚠️ Be aware that starting point might change! Please follow us on
-            Instagram for updates. ⚠️
-          </h3>
-          <Link
-            aria-label="Go to instagram"
-            target="_blank"
-            href="https://www.instagram.com/nrc.int.team/"
-          >
-            <Button
-              placeholder={""}
-              aria-label="Go to instagram"
-              size="lg"
-              name="Instagram"
-              className="bg-gradient-to-tr mb-10 from-yellow-500 via-pink-600 to-purple-700 hover:from-yellow-600 hover:via-pink-700 hover:to-purple-800"
+          <div className="flex items-center gap-2">
+            <Link aria-label="Go to komoot" target="_blank" href={komootUrl}>
+              <Button
+                placeholder={""}
+                aria-label="Go to komoot"
+                size="lg"
+                name="Komoot"
+                style={{ background: "#447c00" }}
+                className="bg-gradient-to-tr from-#8acb3c via-#66ae0e to-#447c00 hover:from-yellow-600 hover:via-pink-700 hover:to-purple-800"
+              >
+                <Image
+                  src={`${
+                    process.env.NEXT_PUBLIC_BASE_URL ?? ""
+                  }/image/komoot.png`}
+                  alt="Komoot"
+                  width={20}
+                  height={20}
+                />
+              </Button>
+            </Link>
+            <h3
+              className="mb-2 text-2xl font-bold "
+              style={{ color: "#447c00" }}
             >
-              <BsInstagram className="text-white text-xl" />
-            </Button>
-          </Link>
+              Komoot Track for 21 June
+            </h3>
+          </div>
+          <br />
           <p className="mb-6">
             🇬🇧 Join us for an open group ride! 🚴‍♀️
             <br /> 📅 Date: Every Saturday <br /> ⏰ Time: 10:00 - 13:00 <br />
@@ -64,6 +75,25 @@ export const SocialRides = () => {
             warten auf jeden! 🌟
           </p>
           <br />
+          <h3 className="mb-2 text-2xl font-bold text-red-600">
+            ⚠️ Be aware that starting point might change! Please follow us on
+            Instagram for updates. ⚠️
+          </h3>
+          <Link
+            aria-label="Go to instagram"
+            target="_blank"
+            href="https://www.instagram.com/nrc.int.team/"
+          >
+            <Button
+              placeholder={""}
+              aria-label="Go to instagram"
+              size="lg"
+              name="Instagram"
+              className="bg-gradient-to-tr mb-10 from-yellow-500 via-pink-600 to-purple-700 hover:from-yellow-600 hover:via-pink-700 hover:to-purple-800"
+            >
+              <BsInstagram className="text-white text-xl" />
+            </Button>
+          </Link>
         </div>
 
         <Image
