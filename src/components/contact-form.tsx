51be8274e57186ca526fc4e50e82871adaf605f7
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-07-18 11:07:43 +0200

    contact form

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -5,10 +5,11 @@ import * as Yup from "yup";
 import emailjs from "@emailjs/browser";
 import toast from "react-hot-toast";
 import { Loader } from "@/components/loader";
+import { Button } from "@material-tailwind/react";
 
 const contactValidationSchema = Yup.object().shape({
-  firstName: Yup.string().required("Please enter your first name"),
-  lastName: Yup.string().required("Please enter your last name"),
+  firstName: Yup.string().nullable(),
+  lastName: Yup.string().nullable(),
   email: Yup.string()
     .email("Invalid email")
     .required("Please enter your email"),
@@ -20,14 +21,14 @@ export const FormSection = () => {
   const [_, setFormSubmitted] = useState(false);
   const formik = useFormik<{
     email: string;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     textarea: string | null;
   }>({
     initialValues: {
       email: "",
-      firstName: "",
-      lastName: "",
+      firstName: null,
+      lastName: null,
       textarea: null,
     },
     validateOnBlur: false,
@@ -67,92 +68,78 @@ export const FormSection = () => {
   });
 
   return (
-    <section className="px-8 relative z-100">
-      <div className="container mx-auto text-center">
-        {/* <h1
-                  color="blue-gray"
-                  className="mb-4 leter-spacing-1 text-5xl font-bold"
-                >
-                  Get Started Today
-                </h1> */}
+    <section className="  list-item bg-cover bg-center bg-no-repeat relative">
+      <div className=" px-8 container mx-auto text-center">
+        <h1
+          color="blue-gray"
+          className="mb-4 leter-spacing-1 text-5xl font-bold"
+        >
+          Contact Us
+        </h1>
         <div className="flex-row justify-center align-middle">
           <div className="flex justify-center mb-6">
-            <h3 className="leter-spacing-1 text-3xl max-w-3xl">
-              DROP A MESSAGE AND WE'LL GET BACK TO YOU WITHIN 24 HOURS
-            </h3>
+            <p className="leter-spacing-1 text-xl max-w-3xl">
+              Leave a request and we will contact you back shortly.
+            </p>
           </div>
           <br />
         </div>
       </div>
 
-      <div
-        style={{ alignItems: "flex-start" }}
-        className="flex-row md:container md:mx-auto md:flex items-center justify-between"
-      >
-        <div className="md:px-8 pt-6 pb-8 mb-4  md:ml-16 w-full">
-          <form onSubmit={formik.handleSubmit}>
+      <div className="flex items-center justify-center">
+        <div className="px-8  pb-8 flex justify-center items-center">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="min-w-[300px] md:min-w-[700px]"
+          >
             <div className="flex justify-between ">
-              <div className="mb-6 w-4/5 mr-10">
+              <div className="mb-8 w-4/5 mr-10">
                 <label
-                  className="block   text-sm font-bold mb-2"
+                  className="block   text-lg font-bold mb-2"
                   htmlFor="text"
                 >
-                  First Name*
+                  First Name
                 </label>
                 <input
-                  className={`shadow appearance-none border ${
-                    formik.errors.firstName && "border-red-500"
-                  } rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline`}
+                  className={`shadow min-h-14 appearance-none border rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline`}
                   id="firstName"
                   type="text"
                   disabled={disabled}
                   name="firstName"
                   placeholder="First Name"
-                  value={formik.values.firstName}
+                  value={formik.values.firstName ?? ""}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                 />
-                {formik.errors.firstName && (
-                  <div className="text-red-500 text-xs italic">
-                    {formik.errors.firstName}
-                  </div>
-                )}
               </div>
               <div className="mb-6 w-4/5">
                 <label
-                  className="block   text-sm font-bold mb-2"
+                  className="block   text-lg font-bold mb-2"
                   htmlFor="text"
                 >
-                  Last Name*
+                  Last Name
                 </label>
                 <input
-                  className={`shadow appearance-none border ${
-                    formik.errors.lastName && "border-red-500"
-                  } rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline`}
+                  className={`shadow min-h-14 appearance-none border rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline`}
                   id="lastName"
                   type="text"
                   disabled={disabled}
                   name="lastName"
                   placeholder="Last Name"
-                  value={formik.values.lastName}
+                  value={formik.values.lastName ?? ""}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                 />
-                {formik.errors.lastName && (
-                  <div className="text-red-500 text-xs italic">
-                    {formik.errors.lastName}
-                  </div>
-                )}
               </div>
             </div>
-            <div className="mb-4">
-              <label className="block text-sm font-bold mb-2" htmlFor="email">
+            <div className="mb-8">
+              <label className="block text-lg font-bold mb-2" htmlFor="email">
                 Email*
               </label>
               <input
                 className={`shadow appearance-none border ${
                   formik.errors.email && "border-red-500"
-                } rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline`}
+                } rounded w-full py-2 px-3   min-h-14  leading-tight focus:outline-none focus:shadow-outline`}
                 id="username"
                 type="email"
                 disabled={disabled}
@@ -169,13 +156,13 @@ export const FormSection = () => {
               )}
             </div>
             <div className="mb-4">
-              <label className="block   text-sm font-bold mb-2" htmlFor="email">
-                Leave your message
+              <label className="block   text-lg font-bold mb-2" htmlFor="email">
+                Leave your message*
               </label>
               <textarea
                 className={`shadow appearance-none border ${
                   formik.errors.textarea && "border-red-500"
-                } rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline min-h-28`}
+                } rounded w-full py-2 px-3   leading-tight focus:outline-none focus:shadow-outline min-h-36`}
                 id="textarea"
                 disabled={disabled}
                 name="textarea"
@@ -190,19 +177,18 @@ export const FormSection = () => {
                 </p>
               )}
             </div>
-            <div className="flex items-center start mt-8">
-              <button
-                className={`w-full  ${
-                  disabled
-                    ? "bg-gray-500"
-                    : "bg-deep-purple-800 hover:bg-deep-purple-400"
-                } text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+            <div className="flex items-center justify-end mt-8">
+              <Button
+                className={` ${
+                  disabled && "bg-gray-500"
+                } text-black px-6 py-3 rounded-xl font-bold transition flex items-center gap-2 shadow-lg`}
+                style={{ background: "#37007d", color: "white" }}
                 type="submit"
                 value="Submit"
                 disabled={disabled}
               >
-                {disabled ? <Loader /> : "Submit"}
-              </button>
+                {disabled ? <Loader /> : <>Submit</>}
+              </Button>{" "}
             </div>
           </form>
         </div>
