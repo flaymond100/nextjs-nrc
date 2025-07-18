630d2b633d5e94a899dc35e77776808570052df9
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-07-18 11:18:00 +0200

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,28 @@ import { Navbar, Footer } from "@/components";
 
 // sections
 import Home from "./home";
-import OurPrograms from "./our-programs";
-import Prices from "./prices";
-import OurServices from "./our-services";
 import Faq from "./faq";
 import CarouselFeatures from "./carousel-features";
 import { CalendlyWidget } from "@/components/widget";
 import { FormSection } from "@/components/contact-form";
+import { Sponsors } from "@/components/sponsors";
 
 export default function Campaign() {
   return (
     <>
       <Navbar />
       <Home />
+      <SponsorsWrapper />
       <FormSection />
-      <OurPrograms />
-      <Prices />
-      <OurServices />
-      <CarouselFeatures />
-      <CalendlyWidget />
-      <Faq />
       <Footer />
     </>
   );
 }
+
+const SponsorsWrapper = () => {
+  return (
+    <div className="container mx-auto px-4 py-12 mb-10">
+      <Sponsors />
+    </div>
+  );
+};
--- src/app/social-rides/page.tsx
+++ src/app/social-rides/page.tsx
@@ -2,21 +2,13 @@
 import { Navbar, Footer } from "@/components";
 
 // sections
-import Faq from "../faq";
-import CarouselFeatures from "../carousel-features";
-import { CalendlyWidget } from "@/components/widget";
 import { SocialRides } from "@/components/social-rides";
-import { PersonalCoaching } from "@/components/personal-coaching";
 
 export default function SocialRidesPage() {
   return (
     <>
       <Navbar />
       <SocialRides />
-      <PersonalCoaching />
-      <CarouselFeatures />
-      <CalendlyWidget />
-      <Faq />
       <Footer />
     </>
   );
