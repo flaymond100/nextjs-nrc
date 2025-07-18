c72cf37d6d1428cd82e2ee238370708835295571
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-07-18 10:56:11 +0200

    remove pricing

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,18 +6,26 @@ import { Navbar, Footer } from "@/components";
 import Faq from "../faq";
 import React from "react";
 import CarouselFeatures from "../carousel-features";
-import { Prices } from "../prices";
 import { FormSection } from "@/components/contact-form";
 
 export default function ContactUs() {
   return (
     <>
       <Navbar />
-      <FormSection />
-      <Prices />
+      <ContactUsPage />
       <CarouselFeatures />
       <Faq />
       <Footer />
     </>
   );
 }
+
+const ContactUsPage = () => {
+  return (
+    <>
+      <section className="container mx-auto px-4 py-12">
+        <FormSection />
+      </section>
+    </>
+  );
+};
