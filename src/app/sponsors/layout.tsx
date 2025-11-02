22c13e916967593eec5dad09dde9ebc40ab8c97c
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-11-02 21:23:19 +0100

    added partners

--- src/app/sponsors/layout.tsx
+++ src/app/sponsors/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 import { Layout } from "@/components";
 
 export const metadata: Metadata = {
-  title: "Sponsors",
+  title: "Partners",
   description:
-    "We’re incredibly grateful for the support of our amazing sponsors. Their contributions empower our team to train harder, race faster, and bring the cycling community together. Thank you for being part of our journey.",
+    "We’re incredibly grateful for the support of our amazing partners. Their contributions empower our team to train harder, race faster, and bring the cycling community together. Thank you for being part of our journey.",
   alternates: {
-    canonical: `https://www.nrc-team.com/sponsors`,
+    canonical: `https://www.nrc-team.com/partners`,
   },
 };
 
