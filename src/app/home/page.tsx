e1f5090886105c4ef4a214754493bfee85a1e2e2
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-12-10 10:02:33 +0100

    fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,7 @@
-// components
-import { Navbar, Footer } from "@/components";
-
-// sections
-import Faq from "../faq";
-import Image from "next/image";
-import CarouselFeatures from "../carousel-features";
-import { CalendlyWidget } from "@/components/widget";
-import Prices from "../prices";
 import { ChooseType } from "@/components/home-choose-type";
 
 export default function AboutUsPage() {
-  return (
-    <>
-      {/* <Navbar /> */}
-      <AboutUs />
-      {/* <Footer /> */}
-    </>
-  );
+  return <AboutUs />;
 }
 
 const AboutUs = () => {
